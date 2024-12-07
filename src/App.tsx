84d36744f263f218e9fe42c1e2e96f4d81f721d1
touchtt0205 <tttouch1122@gmail.com>
2024-12-08 00:51:41 +0700

    fix enpoint to acces

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Canvas from "./components/Canvas";
 const App: React.FC = () => {
 
   return (
-    <Router> 
+    <Router basename="/Starting-Lineup-VC"> 
       <div>
         <Routes>
           <Route path="/" element={<Canvas />} />
